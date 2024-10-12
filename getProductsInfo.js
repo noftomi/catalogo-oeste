d913ba81f6cd22fb7c7feaa5033f510d9256e4b2
tomi <tomasaguirre144@gmail.com>
2024-10-12 19:02:42 -0300

    big update on filters and bd info

--- getProductsInfo.js
+++ getProductsInfo.js
@@ -1,24 +1,27 @@
 async function fetchAndProcessCSV() {
   try {
     const responseCSV = await fetch(
-      'https://docs.google.com/spreadsheets/d/1CFjNITS2MatNET1wth1VaBTfVuTSUtkaMxlPurwdqa8/pub?gid=0&single=true&output=csv'
-    ).then(response => response.text());
+      "https://docs.google.com/spreadsheets/d/1CFjNITS2MatNET1wth1VaBTfVuTSUtkaMxlPurwdqa8/pub?gid=0&single=true&output=csv"
+    ).then((response) => response.text());
 
     const response = responseCSV
-      .split('\n')
+      .split("\n")
       .slice(1)
-      .map(row => {
-        const [id, productName, description, price] = row.split(',');
-        console.log("🚀 ~ fetchAndProcessCSV ~ price:", id,productName,price)
-        return { id, productName, description, price: Number(price) };
+      .map((row) => {
+        const [id, productName, description, price, type, filter_brand] =
+          row.split(",");
+        return {
+          id,
+          productName,
+          description,
+          price: Number(price),
+          type,
+          filter_brand,
+        };
       });
-
-    console.log('🚀 ~ fetchAndProcessCSV ~ response:', response);
 
     return response;
   } catch (error) {
-    console.error('Error fetching or processing CSV:', error);
+    console.error("Error fetching or processing CSV:", error);
   }
 }
-
-
